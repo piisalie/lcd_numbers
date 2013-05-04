637e0bc0bce72bc244324c874c7427e5b3c6096f
Paul Dawson <excidere@gmail.com>
2013-05-04 15:21:52 -0500

    added spec for line count

--- spec/lcd_number_spec.rb
+++ spec/lcd_number_spec.rb
@@ -7,13 +7,17 @@ describe LCDNumber do
     expect(small.to_lcd.lines.first.size).to be < big.to_lcd.lines.first.size
   end
 
+  let(:number) { LCDNumber.new("23") }
+  
   it "outputs at least five lines" do
-    number = LCDNumber.new("23")
     expect(number.to_lcd.lines.to_a.size).to be >= 5
   end
 
   it "defaults to a size of 2" do
-    number = LCDNumber.new("23")
     expect(number.size).to eq(2)
+  end
+
+  it "the number of lines returned is described by the size" do
+    expect(number.to_lcd.lines.to_a.size).to eq((number.size * 2) + 3)
   end
 end

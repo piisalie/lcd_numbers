df92b91e7c4af562c65d957927335a00c20fbb09
James Edward Gray II <james@graysoftinc.com>
2013-05-04 15:31:39 -0500

    Adding a spec for horizontal bars.

--- spec/lcd_number_spec.rb
+++ spec/lcd_number_spec.rb
@@ -1,16 +1,12 @@
 require_relative "../lib/lcd_number"
 
 describe LCDNumber do
+  let(:number) { LCDNumber.new("23") }
+
   it "outputs each digit provided going across" do
     small = LCDNumber.new("1")
     big   = LCDNumber.new("12")
     expect(small.to_lcd.lines.first.size).to be < big.to_lcd.lines.first.size
-  end
-
-  let(:number) { LCDNumber.new("23") }
-  
-  it "outputs at least five lines" do
-    expect(number.to_lcd.lines.to_a.size).to be >= 5
   end
 
   it "defaults to a size of 2" do
@@ -19,5 +15,10 @@ describe LCDNumber do
 
   it "the number of lines returned is described by the size" do
     expect(number.to_lcd.lines.to_a.size).to eq((number.size * 2) + 3)
+  end
+
+  it "adjusts horizontal bars for the size" do
+    expect(number.digits).to match(/[02-9]/)
+    expect(number.to_lcd).to match(/\s-{#{number.size}}\s/)
   end
 end

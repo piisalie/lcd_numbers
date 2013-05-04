5fcba88337f31361d1208b52a42db9b34694b6c3
Paul Dawson <excidere@gmail.com>
2013-05-04 14:57:15 -0500

    increasing size for increasing digits

---
+++ lib/lcd_number.rb
@@ -0,0 +1,9 @@
+class LCDNumber
+  def initialize(number)
+    @number = number
+  end
+
+  def to_lcd
+    return @number
+  end
+end
--- spec/lcd_number_spec.rb
+++ spec/lcd_number_spec.rb
@@ -6,4 +6,9 @@ describe LCDNumber do
     big   = LCDNumber.new("12")
     expect(small.to_lcd.lines.first.size).to be < big.to_lcd.lines.first.size
   end
+
+  it "outputs at least five lines" do
+    number = LCDNumber.new("23")
+    expect(number.to_lcd.lines.to_a.size).to be >= 5
+  end
 end
